31092646bcf3d0f07521b58f1295e082119b6c52
Marcelo Schroeder <marcelo.schroeder@infoaccent.com>
2018-08-28 12:42:44 +1000

    Commit from 2

--- src/Main.groovy
+++ src/Main.groovy
@@ -6,9 +6,13 @@ import static Sum.sum
 def name = "Matthew"
 int programmingPoints = 10
 
+// comment from 2
+
 println "Hello ${name}"
 println "${name} has at least ${programmingPoints} programming points."
 println "${programmingPoints} squared is ${square(programmingPoints)}"
 println "${programmingPoints} divided by 2 bonus points is ${divide(programmingPoints, 2)}"
 println "${programmingPoints} minus 7 bonus points is ${subtract(programmingPoints, 7)}"
 println "${programmingPoints} plus 3 bonus points is ${sum(programmingPoints, 3)}"
+
+// comment fromn 2
