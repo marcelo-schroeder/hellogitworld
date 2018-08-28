e7605173092217458cf884cece63e819cfdff233
Marcelo Schroeder <marcelo.schroeder@infoaccent.com>
2018-08-28 12:53:33 +1000

    Commit from 1

--- src/Division.groovy
+++ src/Division.groovy
@@ -1,4 +1,6 @@
 static int divide(int val1, val2) {
     val1 / val2
 }
+// comment from 1
+
 // comment from 2
--- src/Main.groovy
+++ src/Main.groovy
@@ -15,5 +15,8 @@ println "${programmingPoints} divided by 2 bonus points is ${divide(programmingP
 println "${programmingPoints} minus 7 bonus points is ${subtract(programmingPoints, 7)}"
 println "${programmingPoints} plus 3 bonus points is ${sum(programmingPoints, 3)}"
 
+// comment from 1
+
+
 // comment fromn 2
 // another comment from 2
